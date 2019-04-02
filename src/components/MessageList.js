500c3546b5cde41305af953e53dab8183ff625cc
bdange <benjamin.dange@gmail.com>
2019-04-02 18:35:00 +0200

    cannot delete msgs

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -5,48 +5,51 @@ class MessageList extends Component {
     super(props);
     this.state = {
       messages: [],
-      username: '',
-      content: '',
-      sentAt: '',
-      roomId: ''
-    };  
+      username: "",
+      content: "",
+      sentAt: "",
+      roomId: ""
+    };
 
     this.messagesRef = this.props.firebase.database().ref("messages");
     this.handleChange = this.handleChange.bind(this);
     this.createMessage = this.createMessage.bind(this);
   }
 
-
-  componentDidMount(){
-    this.messagesRef.on('child_added', snapshot => {
+  componentDidMount() {
+    this.messagesRef.on("child_added", snapshot => {
       const message = snapshot.val();
       message.key = snapshot.key;
-      this.setState({messages: this.state.messages.concat(message)})
-      });
-    }
-
+      this.setState({ messages: this.state.messages.concat(message) });
+    });
+  }
 
   createMessage() {
-    this.messagesRef.push ({
+    this.messagesRef.push({
       content: this.state.newMessage,
       roomId: this.props.activeRoom.key,
       sentAt: this.props.firebase.database.ServerValue.TIMESTAMP,
       username: this.props.username
     });
-}
+  }
 
-  handleSubmit(e){
+  handleSubmit(e) {
     e.preventDefault();
-    if (!this.state.newMessage){
+    if (!this.state.newMessage) {
       return;
     }
     this.createMessage(this.state.newMessage);
-    this.setState({newMessage: ''});
+    this.setState({ newMessage: "" });
   }
 
-  handleChange(e){
-    const newName =  e.target.value;
-    this.setState({newMessage: newName});
+  handleChange(e) {
+    const newName = e.target.value;
+    this.setState({ newMessage: newName });
+  }
+
+  deleteMessage(message) {
+    let newRef = this.messagesRef(message.index);
+    newRef.remove();
   }
 
   render() {
@@ -63,28 +66,34 @@ class MessageList extends Component {
                     {new Date(message.sentAt).toString()}
                     {message.username}
                     {message.content}
+                    <button
+                      className="delete"
+                      onClick={() => this.deleteMessage(message)}
+                    >
+                      delete
+                    </button>
                   </li>
                 );
               })}
           </ul>
         </section>
         <section>
-        <div className="create-message" >
-          <form onSubmit={e => this.handleSubmit(e)}>
-            <input
-            type="text"
-            value={this.state.newMessage}
-            onChange={e=> this.handleChange(e)}
-            className="newMessage"
-            placeholder="Send a new message"
-            />
-            <input type="submit" value="send" />
-         </form>
-         </div>
-         </section>
+          <div className="create-message">
+            <form onSubmit={e => this.handleSubmit(e)}>
+              <input
+                type="text"
+                value={this.state.newMessage}
+                onChange={e => this.handleChange(e)}
+                className="newMessage"
+                placeholder="Send a new message"
+              />
+              <input type="submit" value="send" />
+            </form>
+          </div>
+        </section>
       </div>
     );
-  };
+  }
 }
 
 export default MessageList;
